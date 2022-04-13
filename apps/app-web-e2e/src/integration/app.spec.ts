296da6e2188b33c12d4750a71e825ff59db8f272
Mike Kelly <mike@curioushuman.com.au>
2022-04-13 11:25:21 +1000

    updating name of next app

--- apps/app-web-e2e/src/integration/app.spec.ts
+++ apps/app-web-e2e/src/integration/app.spec.ts
@@ -1,6 +1,6 @@
 import { getGreeting } from '../support/app.po';
 
-describe('next-app', () => {
+describe('app-web', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
@@ -8,6 +8,6 @@ describe('next-app', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome next-app');
+    getGreeting().contains('Welcome app-web');
   });
 });
--- apps/app-web/pages/index.tsx
+++ apps/app-web/pages/index.tsx
@@ -13,7 +13,7 @@ export function Index() {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome next-app 👋
+              Welcome app-web 👋
             </h1>
           </div>
 
