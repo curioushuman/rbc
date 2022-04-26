f0586633e46e6b207b25bb4459aa6be6a4475b1f
Mike Kelly <mike@curioushuman.com.au>
2022-04-26 19:00:19 +1000

    feat(api): :sparkles: Reintroducing custom logger

--- apps/api/src/bootstrap/bootstrap.ts
+++ apps/api/src/bootstrap/bootstrap.ts
@@ -1,8 +1,11 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 
+import { LoggableLogger } from '@curioushuman/loggable';
+
 export class Bootstrap {
   public static useGlobalSettings(app: INestApplication) {
     app.setGlobalPrefix('api');
     app.useGlobalPipes(new ValidationPipe());
+    app.useLogger(new LoggableLogger());
   }
 }
